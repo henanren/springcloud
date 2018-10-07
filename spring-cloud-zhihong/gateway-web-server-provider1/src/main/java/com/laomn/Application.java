90174922bc70339e3bf25c42bc1511c05b8fcbd1
henanren <316936460@qq.com>
2018-10-07 19:11:34 +0800

    ti jiao

--- spring-cloud-zhihong/gateway-web-server-provider1/src/main/java/com/laomn/Application.java
+++ spring-cloud-zhihong/gateway-web-server-provider1/src/main/java/com/laomn/Application.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.laomn.eureka.provider.EurekaProvider;
-
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaProvider.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
